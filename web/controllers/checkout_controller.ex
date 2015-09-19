d8fc2b98aa77fa5df2e55153df14b1eef94c06b5
Josh Nussbaum <joshnuss@gmail.com>
2015-09-18 20:41:17 -0400

    Added params parsing and error handling

--- web/controllers/checkout_controller.ex
+++ web/controllers/checkout_controller.ex
@@ -12,8 +12,47 @@ defmodule Store.CheckoutController do
   end
 
   def create(conn, params) do
-    IO.inspect(params)
+    card    = get_card(params)
+    address = get_address(params)
 
-    render conn, "thank-you.html"
+    response = Billing.authorize(:default_gateway, 199.95, card,
+                                 billing_address: address,
+                                 description: "T-Shirt")
+
+    respond(conn, response,
+      card: card,
+      address: address)
+  end
+
+  defp respond(conn, {:error, response}, assigns) do
+    assigns = Keyword.put(assigns, :error, response.raw["error"]["message"])
+
+    render conn, "index.html", assigns
+  end
+
+  defp respond(conn, {:ok, response}, _) do
+    render conn, "thank-you.html", response: response
+  end
+
+  defp get_card(params) do
+    %Billing.CreditCard{name:       params["name"],
+                        number:     params["card"],
+                        expiration: get_expiration(params["year"], params["month"]),
+                        cvc:        params["cvc"]}
+  end
+
+  defp get_expiration(year, month) when byte_size(year) > 0 and byte_size(month) > 0,
+    do: {String.to_integer(year), String.to_integer(month)}
+
+  defp get_expiration(_, _),
+    do: {0, 0}
+
+  defp get_address(params) do
+    %Billing.Address{street1:     params["street1"],
+                     street2:     params["street2"],
+                     city:        params["city"],
+                     region:      params["region"],
+                     country:     params["country"],
+                     postal_code: params["postal_code"]}
   end
 end
