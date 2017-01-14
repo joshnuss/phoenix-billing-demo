96e30b0b6d220aab0d3b69f5f39abf3dfcff989a
Josh Nussbaum <joshnuss@gmail.com>
2017-01-14 01:14:22 -0500

    Remove warning

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Store.Mixfile do
      compilers: [:phoenix] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps()]
   end
 
   # Configuration for the OTP application
